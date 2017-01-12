25f2dc11d9d7f55b9739916395cad55e1a9a213e
Scott Zuehlke <zuehlkescott5@gmail.com>
2017-01-12 00:12:26 -0600

    Added updates to UI

--- R/RiddleCorner/app.R
+++ R/RiddleCorner/app.R
@@ -1,23 +1,21 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+date1 <- Sys.Date()
+
+
 ui <- fluidPage(
   
   mainPanel(
     img(src = "ButtersSmile.png", height = 150, width = 150),
     div("Scott's Riddle Corner!",style = "font-family: 'times'; font-size:48pt; color:red"),
-    textInput("riddle", "Riddle Me This...",value="???")
+    dateInput("date1", "Date:",value = date1, format = "mm/dd/yyyy"),
+    textInput("riddle", "This week's riddle:",value="???"),
+    textInput("riddle", "What's your answer?",value="Give it your best shot.")
+  )
 )
-)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output,session) {
@@ -26,14 +24,7 @@ server <- function(input, output,session) {
     stopApp()
   })
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+
 }
 
 # Run the application 
